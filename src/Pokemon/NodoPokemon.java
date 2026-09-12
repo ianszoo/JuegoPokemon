1090ed82bfd0e7182f42725f76221f15000d73e6
Ian Suazo <ianantoniosuazo@gmail.com>
2026-09-12 08:47:00 -0600

    Se crea la clase de NodoPokemon para poder trabajar con la lista enlazada del pokemon

--- src/Pokemon/NodoPokemon.java
+++ src/Pokemon/NodoPokemon.java
@@ -10,5 +10,30 @@ package Pokemon;
  * @author Ian Suazo Palao
  */
 public class NodoPokemon {
+    private Pokemon pokemon;
+    private NodoPokemon siguiente;
+
+    public NodoPokemon(Pokemon pokemon) {
+        this.pokemon = pokemon;
+        this.siguiente=null;
+        
+    }
+    
+    public Pokemon getPokemon(){
+        return pokemon;
+    }
+    
+    public void setPokemon(Pokemon pokemon){
+        this.pokemon = pokemon;
+    }
+
+    public NodoPokemon getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoPokemon siguiente) {
+        this.siguiente = siguiente;
+    }
+    
     
 }
